643a03c28d6e044fac2391acd891b5d88aaf4380
Petar Petkov <petar.b.petkov@gmail.com>
2015-11-03 22:38:42 +0400

    Added admin contests edit action. small fixes to the regular edit contest action

--- PhotoContest/PhotoContest.App/Areas/Admin/Controllers/ContestsController.cs
+++ PhotoContest/PhotoContest.App/Areas/Admin/Controllers/ContestsController.cs
@@ -2,6 +2,7 @@
 {
     #region
 
+    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Web.Mvc;
@@ -10,6 +11,7 @@
 
     using Ninject.Infrastructure.Language;
 
+    using PhotoContest.App.Models.Photos.Contests;
     using PhotoContest.App.Models.Photos.Users;
     using PhotoContest.App.Models.ViewModels.Contests;
     using PhotoContest.Data.Interfaces;
@@ -41,9 +43,43 @@
             return this.View(contests);
         }
 
-        public ActionResult Edit()
+        public ActionResult Edit(int? id)
         {
-            return this.View();
+            var contest = this.Data.Contests.All().SingleOrDefault(c => c.Id == id);
+            if (contest == null)
+            {
+                return this.RedirectToAction("ViewAll", "Contests");
+            }
+            return this.View(contest);
+        }
+
+        [HttpPost]
+        public ActionResult SaveContest(ContestBindingModel updatedContest)
+        {
+            if (!this.ModelState.IsValid || updatedContest == null)
+            {
+                throw new ArgumentException("Invalid model");
+            }
+
+            var contest = this.Data.Contests.All().FirstOrDefault(a => a.Id == updatedContest.Id);
+
+            if (contest == null)
+            {
+                return this.HttpNotFound("No such contest");
+            }
+
+            contest.Description = updatedContest.Description;
+            contest.MaxNumberOfParticipants = updatedContest.MaxNumberOfParticipants;
+            contest.Status = updatedContest.Status;
+            contest.Title = updatedContest.Title;
+            contest.RewardStrategy = updatedContest.RewardStrategy;
+            contest.Deadline = updatedContest.Deadline;
+            contest.DeadlineStrategy = updatedContest.DeadlineStrategy;
+            contest.VotingStrategy = updatedContest.VotingStrategy;
+
+            this.Data.SaveChanges();
+
+            return this.RedirectToAction("ViewAll", "Contests", new { area = "Admin" });
         }
 
         public ActionResult Delete()
