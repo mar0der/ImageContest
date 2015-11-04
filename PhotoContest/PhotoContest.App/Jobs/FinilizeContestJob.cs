c5fad112128fdedbf63a6f8ada9b4385fc0756b5
dhadzhiew <dhadzhiew@gmail.com>
2015-11-04 14:09:17 +0200

    finilize contest job, tests project

--- PhotoContest/PhotoContest.App/Jobs/FinilizeContestJob.cs
+++ PhotoContest/PhotoContest.App/Jobs/FinilizeContestJob.cs
@@ -1,5 +1,7 @@
 ﻿namespace PhotoContest.App.Jobs
 {
+    using PhotoContest.App.Controllers;
+    using PhotoContest.Data.Interfaces;
     using Quartz;
     using Quartz.Impl;
     using System;
@@ -11,9 +13,24 @@
 
     public class FinilizeContestJob : IJob
     {
+        public IPhotoContestData Data { get; set; }
+        public FinilizeContestJob(IPhotoContestData data)
+        {
+            this.Data = data;
+        }
+
         public void Execute(IJobExecutionContext context)
         {
-            
+            var contests = this.Data.Contests.All().Where(c => c.Deadline <= DateTime.Now);
+
+            if (contests.Count() > 0)
+            {
+                var contestController = new ContestsController(this.Data);
+                foreach (var contest in contests)
+                {
+                    contestController.FinalizeContest(contest.Id);
+                }
+            }
         }
     }
 }
